64bb2f22d8ac457faf4814f6cab68cb235af603a
TheCactusBlue <thecactusblue@gmail.com>
2024-07-22 23:43:29 +1000

    could have a better API, but this works for now

--- mikoto-server/src/main.rs
+++ mikoto-server/src/main.rs
@@ -7,14 +7,14 @@ pub enum Error {
     NotFound,
 }
 
-impl From<hyperschema::error::Error> for Error {
-    fn from(_: hyperschema::error::Error) -> Self {
-        Error::InternalServerError
+impl From<Error> for hyperschema::error::Error {
+    fn from(_: Error) -> Self {
+        hyperschema::error::Error::InternalServerError
     }
 }
 
 #[derive(Serialize, Deserialize, Type)]
-pub struct Person {
+pub struct Human {
     pub name: String,
     pub age: i32,
     pub pet: Option<Animal>,
@@ -26,26 +26,31 @@ pub struct Animal {
     pub age: i32,
 }
 
-fn main() {
-    let person_service = Service::<()>::new("PersonService")
+fn humans() -> Service<()> {
+    Service::<()>::new("HumanService")
         .query("get", |_, name: String| async move {
-            Ok(Person {
+            Ok(Human {
                 name,
                 age: 22,
                 pet: None,
             })
         })
         .procedure("set", |_, name: String| async move {
-            Ok(Person {
+            if name.is_empty() {
+                return Err(Error::NotFound.into());
+            }
+            Ok(Human {
                 name,
                 age: 22,
                 pet: None,
             })
-        });
+        })
+}
 
-    let service = Service::<()>::new("Mikoto")
+fn main() {
+    let service = Service::<()>::new("MikotoClient")
         .query("ping", |_, pong: String| async move { Ok(pong) })
-        .mount("persons", person_service);
+        .mount("humans", humans());
 
     let ts = TypeScriptGenerator::new(ExportConfig::default(), &service).generate();
     println!("{}", ts);
